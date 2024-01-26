cce738cdee6160e8ee808772112a2922f67386a7
JvLima <87541415+jvlima1205@users.noreply.github.com>
2024-01-26 14:56:32 -0300

    aula16-ex complete

---
+++ aula16-ex/script.js
@@ -0,0 +1,63 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+        
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    }else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        let media = soma / total
+        
+        res.innerHTML = ''
+        res.innerHTML = `<p>Ao todo temos <span class="valores">${total}</span> números cadastrados</p>
+        <p>O maior valor informado foi <span class="valores">${maior}</span></p>
+        <p>O menor valor informado foi <span class="valores">${menor}</span></p>
+        <p>Somando todos os valores temos <span class="valores">${soma}</span></p>
+        <p>A média dos valores digitados é <span class="valores">${media}</span></p>
+        `
+    }
+}
